97c89184b0db3e32060bbf4fbbaa737c111fe059
gruntkiller247 <einbindermattwork@gmail.com>
2026-03-18 17:08:53 -0400

    Player HP UI WORKS!

--- src/game.c
+++ src/game.c
@@ -25,147 +25,6 @@ static int viewHeight = 720;
 static int renderWdith = 1200;
 static int renderHeight = 720;
 
-//This code was stolen from some fucking person on stack overflow and it works for everyone but me!
-//https://stackoverflow.com/questions/22886500/how-to-render-text-in-sdl2
-void stolenCodeThatClaimsToWorkButDoesNot()
-{
-    //this opens a font style and sets a size
-    TTF_Font* Sans = TTF_OpenFont("Sans.ttf", 24);
-
-    // this is the color in rgb format,
-    // maxing out all would give you the color white,
-    // and it will be your text's color
-    SDL_Color White = { 255, 255, 255 };
-
-    // as TTF_RenderText_Solid could only be used on
-    // SDL_Surface then you have to create the surface first
-    SDL_Surface* surfaceMessage =
-        TTF_RenderText_Solid(Sans, "put your text here", White);
-
-    // now you can convert it into a texture
-    SDL_Texture* Message = SDL_CreateTextureFromSurface(gf2d_graphics_get_renderer(), surfaceMessage);
-
-    SDL_Rect Message_rect; //create a rect
-    Message_rect.x = 0;  //controls the rect's x coordinate 
-    Message_rect.y = 0; // controls the rect's y coordinte
-    Message_rect.w = 100; // controls the width of the rect
-    Message_rect.h = 100; // controls the height of the rect
-
-    // (0,0) is on the top left of the window/screen,
-    // think a rect as the text's box,
-    // that way it would be very simple to understand
-
-    // Now since it's a texture, you have to put RenderCopy
-    // in your game loop area, the area where the whole code executes
-
-    // you put the renderer's name first, the Message,
-    // the crop size (you can ignore this if you don't want
-    // to dabble with cropping), and the rect which is the size
-    // and coordinate of your texture
-    SDL_RenderCopy(gf2d_graphics_get_renderer(), Message, NULL, &Message_rect);
-    
-    // Don't forget to free your surface and texture
-    SDL_FreeSurface(surfaceMessage);
-    SDL_DestroyTexture(Message);
-}
-
-void realUI(Entity* player, Entity* boss, SDL_Surface* words)
-{
-    if (!words)
-    {
-        slog("Words are Null! Error with rendering text to surface!");
-        return;
-    }
-        
-
-}
-
-
-//This function is meant to draw the HP bars for both the Boss, if any, and the Player dyanamically, but the stupid fucking code does not fucking work and I am sick of it!
-//Instead, I manually mapped out the numbers and drew them with lines. Horseshit and should have never needed to be done!
-void drawUI(Entity* player, Entity* boss, TTF_Font* font)
-{
-    if (!font)
-    {
-        slog("Font does not exist!");
-        return;
-    }
-
-    double* wid, hei;
-    SDL_Color color = { 255, 0, 0, 255 };
-    SDL_Texture* textImage = NULL;
-    SDL_Surface* surface = NULL;
-    //SDL_Texture* Final = NULL;
-
-    GFC_Rect menu;
-
-    
-    
-    //SDL_SetRenderDrawColor(gf2d_graphics_get_renderer(), 255, 255, 255, 255);
-    //SDL_RenderClear(gf2d_graphics_get_renderer());
-
-
-    surface = TTF_RenderText_Blended(font, "Test Text", color);
-
-    if (!surface)
-    {
-        slog("Surface failed: %s\n", TTF_GetError());
-        goto fail;
-    }
-
-    wid = surface->w;
-    hei = surface->h;
-
-    //surface = gf2d_graphics_screen_convert(&surface);
-
-    if (!surface)
-    {
-        slog("Conversion lead to NULL!");
-        goto fail;
-    }
-    
-    textImage = SDL_CreateTextureFromSurface(gf2d_graphics_get_renderer(), surface);
-    SDL_FreeSurface(surface);
-
-    if (!textImage)
-    {
-        slog("Failed to make new texture Error:%s",SDL_GetError());
-        goto fail;
-    }
-    
-    
-
-    menu.x = 0;
-    menu.y = 0;
-    menu.w = (int)wid;
-    menu.h = hei;
-
-    gf2d_draw_rect_filled(menu, GFC_COLOR_WHITE);
-    SDL_RenderCopy(gf2d_graphics_get_renderer(), textImage, NULL, &menu);
-    SDL_RenderPresent(gf2d_graphics_get_renderer());
-    
-    //SDL_Delay(100);
-
-    //SDL_DestroyTexture(textImage);
-    
-    return;
-
-    fail:
-    
-    if (!surface)
-        ;
-    else
-        SDL_FreeSurface(surface);
-
-    if (!textImage)
-        ;
-    else
-        SDL_DestroyTexture(textImage);
-;
-
-    return;
-}
-
 
 //128 x 128 grid for GIMP + snap to grid
 int main(int argc, char * argv[])
@@ -182,7 +41,7 @@ int main(int argc, char * argv[])
 
     srand(time(NULL));
     TTF_Font* font = NULL;
-
+    char playerHP[100];
 
     //slog("The curret time is %i",time(NULL));
     //SDL_GetTikcs returns milli seconds program has been running
@@ -316,15 +175,17 @@ int main(int argc, char * argv[])
              slog("No Level Loaded!");
     }
 
+    snprintf(playerHP, sizeof(playerHP), "Player HP: %d", player->hp);
+
     SDL_Color color = { 255, 255, 255, 255 };
-    SDL_Surface* surface = TTF_RenderText_Solid(font, "Hello SDL_ttf!", color);
+    SDL_Surface* surface = TTF_RenderText_Solid(font, playerHP, color);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(gf2d_graphics_get_renderer(), surface);
 
     SDL_Rect dstRect;
-    dstRect.x = 100;
-    dstRect.y = 100;
-    dstRect.w = surface->w;
-    dstRect.h = surface->h;
+    dstRect.x = 10;
+    dstRect.y = 10;
+    dstRect.w = surface->w/2;
+    dstRect.h = surface->h/2;
     SDL_FreeSurface(surface);
     
    
@@ -384,8 +245,9 @@ int main(int argc, char * argv[])
            
             //drawUI(player, boss, font);
             //realUI(player, boss, surface);
+
             SDL_SetRenderDrawColor(gf2d_graphics_get_renderer(), 0, 0, 0, 255);
-            SDL_RenderClear(gf2d_graphics_get_renderer());
+            //SDL_RenderClear(gf2d_graphics_get_renderer());
 
             // Render text
             SDL_RenderCopy(gf2d_graphics_get_renderer(), texture, NULL, &dstRect);
@@ -423,6 +285,7 @@ int main(int argc, char * argv[])
 
    
     
+    SDL_DestroyTexture(texture);
     TTF_CloseFont(font);
     TTF_Quit();
     entityManagerClose();
